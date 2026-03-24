55473cfdbf637a61a23db465eff8b69ba92165a1
Sahadat Hossen <sahadatjh@gmail.com>
2026-03-24 11:59:51 +0600

    User Dashborad UI     - User Layout UI     - User logout confirmation UI

--- src/app/user/logout/page.tsx
+++ src/app/user/logout/page.tsx
@@ -1,10 +1,24 @@
-import LogoutConfirmationCard from "@/components/admin/logout/LogoutConfirmationCard";
-import UserLayout from "@/components/user/layout/UserLayout.component";
+"use client";
 
-export default function LogoutConfirmation() {
+import UserLayout from "@/components/user/layout/UserLayout";
+import LogoutConfirmation from "@/components/user/logout/Logoutconfirmation";
+import { useRouter } from "next/navigation";
+
+export default function LogoutPage() {
+	const router = useRouter();
+
+	const handleCancel = () => {
+		router.back();
+	};
+
+	const handleConfirm = () => {
+		// Add your logout logic here (clear session, cookies, etc.)
+		router.push("sign-in");
+	};
+
 	return (
-		<UserLayout>
-			<LogoutConfirmationCard />
+		<UserLayout pageTitle="Log out">
+			<LogoutConfirmation onCancel={handleCancel} onConfirm={handleConfirm} />
 		</UserLayout>
 	);
 }
