af98a9486ef501ce6571bb18831402ff2b4debe0
rakib3719 <bannah76769@gmail.com>
2026-03-24 17:53:26 +0600

    Feature: CTA Section done

--- src/app/(website)/cloud-storage/page.tsx
+++ src/app/(website)/cloud-storage/page.tsx
@@ -1,8 +1,10 @@
 import CloudFeatures from '@/components/website/cloud-storage/CloudFeatures';
 import CloudStorageAboutPage from '@/components/website/cloud-storage/CloudStorageAboutPage';
 import CloudStorageBanner from '@/components/website/cloud-storage/CloudStorageBanner';
+import CTASection from '@/components/website/cloud-storage/CTASecton';
 import FilmSection from '@/components/website/cloud-storage/FilmSection';
 import MemoriesSection from '@/components/website/cloud-storage/MemoriesSection';
+import MessageSection from '@/components/website/cloud-storage/MessageSection';
 import { WebLayout } from '@/components/website/layouts/WebLayout';
 
 const page = () => {
@@ -13,6 +15,8 @@ const page = () => {
 <CloudFeatures/>
 <FilmSection/>
 <MemoriesSection/>
+<MessageSection/>
+<CTASection/>
   </WebLayout>
   );
 };
