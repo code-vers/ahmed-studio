35bf67e8ef70ad11e38b4a4735e54706a290099c
rakib3719 <bannah76769@gmail.com>
2026-03-24 17:08:38 +0600

    Feature: Cloud storage about page done

--- src/app/(website)/cloud-storage/page.tsx
+++ src/app/(website)/cloud-storage/page.tsx
@@ -1,3 +1,4 @@
+import CloudStorageAboutPage from '@/components/website/cloud-storage/CloudStorageAboutPage';
 import CloudStorageBanner from '@/components/website/cloud-storage/CloudStorageBanner';
 import { WebLayout } from '@/components/website/layouts/WebLayout';
 
@@ -5,6 +6,7 @@ const page = () => {
   return (
   <WebLayout>
 <CloudStorageBanner/>
+<CloudStorageAboutPage/>
   </WebLayout>
   );
 };
--- src/components/website/cloud-storage/CloudStorageAboutPage.tsx
+++ src/components/website/cloud-storage/CloudStorageAboutPage.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 
 const CloudStorageAboutPage = () => {
   return (
-    <div>
-      
+    <div className="py-12  mx-auto px-28">
+    <h1 className='heading text-center'>About Cloud Storage</h1>
+    <h6 className='subtitle mt-8'>Imagine if we took all your old home movies; cine film, video and camcorder tapes, and we remastered them into HD and even 4K films. Now imagine you can access and share these amazing memories from your Computer, iPad, Phone, Smart TV… Anytime, Anywhere!. Videos uploaded to Ahmed Studio Cloud are available to watch in stunning HD. Get ready for a game-changing experience with our cutting-edge Ai technology! Say goodbye to hours of endless scrolling through your home movies to find that special moment. The Ai bot in the Ahmed Studio Cloud automatically labels and organizes every detail of your videos, from people and objects to text, actions, and sounds. With just a click, you can easily search and access every memory. Relive your favorite moments like never before. It’s time to experience your home movies in style!</h6>
     </div>
   );
 };
